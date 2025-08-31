1de78e0bc84a34e83f528f509308f1b04e8f6c23
CodeWithRaihan <islamraihanul386@gmail.com>
2025-09-01 01:11:06 +0600

    added the js functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-document.getElementById("")
+// login button functionality
+
+document.getElementById("login-btn")
+.addEventListener("click",function(e){
+     e.preventDefault();
+    const mobileNumber = 12345678910;
+    const mobileNumberValue=document.getElementById("mobile-number").value
+    const mobileNumberValueConverted= parseInt(mobileNumberValue)
+
+    const pinNumber=1234;
+    const pinNumberValue= document.getElementById("pin-number").value
+    const pinNumberValueConverted=parseInt(pinNumberValue)
+
+    // condition 
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+          window.location.href="./home.html";
+    }
+    else{
+         alert("Invalid credentials");
+    } 
+    
+})
