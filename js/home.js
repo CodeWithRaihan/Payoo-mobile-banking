a581a3001bd974f46bfa018104e625f30a302354
CodeWithRaihan <islamraihanul386@gmail.com>
2025-09-03 19:38:55 +0600

    added the cashout section functionality

--- js/home.js
+++ js/home.js
@@ -25,14 +25,54 @@
       const addAmountOutput=titleAmount+addAmount;
       titleAmountElement.innerText=addAmountOutput;
 
-      console.log(addAmountOutput);
+      
       
   })
 
+//   cashout money functionality
+  document.getElementById("withdraw-btn")
+  .addEventListener('click',function(e){
+      e.preventDefault()
+
+      const agentNumber=document.getElementById("agent-number-input").value;
+      if(agentNumber.length < 11){
+          alert("Enter 11 digit valid agent number");
+          return;
+      }
+       
+       const cashoutpinNumber=parseInt(document.getElementById("cashout-pin-number").value);
+     if(cashoutpinNumber !== validPin){
+            alert("Enter 4 digit valid pin number");
+            return;
+     }
+      const cashOuntInput=parseInt(document.getElementById("cashout-input-amount").value);
+    //   copy form add money section
+        const titleAmountElement=document.getElementById("title-amount");
+      const titleAmount=parseInt(titleAmountElement.innerText);
+
+      const addAmountOutput=titleAmount - cashOuntInput;
+      titleAmountElement.innerText=addAmountOutput;
+      
+
+      
+  })
 
 
 //   togoling functionality
 
 
-  
-  
+  document.getElementById("add-money-box-btn")
+  .addEventListener('click',function (){
+    console.log("add money btn worked")
+
+     document.getElementById("add-money-sec").style.display = "block";
+     document.getElementById("cash-out-sec").style.display = "none";
+  })
+  document.getElementById("cash-out-box-btn")
+  .addEventListener('click',function(){
+      document.getElementById("cash-out-sec").style.display = "block";
+      document.getElementById("add-money-sec").style.display = "none";
+    console.log("cashout btn worked")
+  })
+
+
